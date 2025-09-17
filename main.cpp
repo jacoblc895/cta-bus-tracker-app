80f32e17b554405635f6c717e1fc2104f4b1365d
jacoblc895 <jacoblc895@gmail.com>
2025-09-17 16:27:55 +0900

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,93 @@
+#include <iostream>
+#include <string>
+
+#include "building.h"
+#include "buildings.h"
+#include "node.h"
+#include "nodes.h"
+#include "busstop.h"
+#include "busstops.h"
+#include "osm.h"
+#include "tinyxml2.h"
+#include "curl_util.h"
+
+using namespace std;
+using namespace tinyxml2;
+
+
+//
+// main
+//
+int main()
+{
+  XMLDocument xmldoc;
+  Nodes nodes;
+  Buildings buildings;
+  
+  cout << "** NU open street map **" << endl;
+
+  string filename;
+
+  cout << endl;
+  cout << "Enter map filename> " << endl;
+  getline(cin, filename);
+
+  //
+  // 1. load XML-based map file 
+  //
+  if (!osmLoadMapFile(filename, xmldoc))
+  {
+    // failed, error message already output
+    return 0;
+  }
+  
+  nodes.readMapNodes(xmldoc);
+  buildings.readMapBuildings(xmldoc);
+
+  BusStops busstops("bus-stops.txt");
+
+  CURL* curl = curl_easy_init();
+  if(curl == nullptr){
+    cout << "**ERROR:" << endl;
+    cout << "**ERROR: unable to initialize curl library" << endl;
+    cout << "**ERROR:" << endl;
+    return 0;
+  }
+  cout << "# of nodes: " << nodes.getNumMapNodes() << endl;
+  cout << "# of buildings: " << buildings.getNumMapBuildings() << endl;
+  cout << "# of bus stops: " << busstops.getNumMapBusStops() << endl;
+
+  while (true)
+  {
+    string name;
+
+    cout << endl;
+    cout << "Enter building name (partial or complete), or * to list, or @ for bus stops, or $ to end> " << endl;
+
+    getline(cin, name);
+
+    if (name == "$") {
+      break;
+    }
+    else if (name == "*") {
+      buildings.print();
+    }
+    else if (name == "@"){
+      busstops.print();
+    }
+    else {
+      buildings.findAndPrint(name, nodes, busstops, curl);
+    }
+
+  }//while
+
+  //
+  // done:
+  //
+
+  curl_easy_cleanup(curl);
+  cout << endl;
+  cout << "** Done **" << endl;
+
+  return 0;
+}
